81a0ecabb062a38122280c476b9523c8067ee393
Atmayanti <mayablue74@gmail.com>
2021-09-23 17:36:25 +0700

    add file processor.java

---
+++ src/jti/relasiclass/pertemuan4/Processor.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jti.relasiclass.pertemuan4;
+
+/**
+ *
+ * @author ATMAYANTI
+ */
+public class Processor {
+    
+}
