91eb0059a25252e4f42f40d868cbbc4c3b334d1a
Atmayanti <mayablue74@gmail.com>
2021-10-07 08:24:07 +0700

    add file percobaan4 perc4

--- src/pertemuan6/percobaan4/ClassB.java
+++ src/pertemuan6/percobaan4/ClassB.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pertemuan6.percobaan4;
 
-/**
- *
- * @author ATMAYANTI
- */
-public class ClassB {
-    
+public class ClassB extends ClassA{
+    ClassB() {
+        System.out.println("konstruktor B dijalankan");
+    }
 }
