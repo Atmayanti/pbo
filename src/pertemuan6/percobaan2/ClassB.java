7b931007155f3c3ed258338e5f09e384cc480b54
Atmayanti <mayablue74@gmail.com>
2021-10-07 07:48:46 +0700

    add file percobaan2 perc2

--- src/pertemuan6/percobaan2/ClassB.java
+++ src/pertemuan6/percobaan2/ClassB.java
@@ -1,14 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pertemuan6.percobaan2;
-
-/**
- *
- * @author ATMAYANTI
- */
 public class ClassB {
+    private int z;
+
+    public void setZ(int z) {
+        this.z = z;
+    }
+
+    public void getNilaiZ() {
+        System.out.println("nilai z:"+z);
+    }
     
+    public void getJumlah(){
+        System.out.println("junlah:"+ (x+y+z));
+    }
 }
