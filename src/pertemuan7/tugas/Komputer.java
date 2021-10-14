3a0602a8f8e7f28458138a8dd99c615c1239ab42
Atmayanti <mayablue74@gmail.com>
2021-10-14 09:15:20 +0700

    add file komputer.java

---
+++ src/pertemuan7/tugas/Komputer.java
@@ -0,0 +1,29 @@
+package pertemuan7.tugas;
+
+public class Komputer {
+    public String merk, jnsProsesor;
+    public int kecProsesor, sizeMemory;
+    
+    public Komputer() {
+    }
+
+    public Komputer(String merk, String jnsProsesor, int kecProsesor, int sizeMemory) {
+        this.merk = merk;
+        this.jnsProsesor = jnsProsesor;
+        this.kecProsesor = kecProsesor;
+        this.sizeMemory = sizeMemory;
+    }
+    
+    public void tampilData(){
+        System.out.println("Merk\t\t\t: "+merk);
+        System.out.println("Kecepatan Processor\t: "+kecProsesor);
+        System.out.println("Size Memory\t\t: "+sizeMemory);
+        System.out.println("Jenis Processor\t\t: "+jnsProsesor);
+    }
+    
+    public static void main(String[] args) {
+        Komputer k = new Komputer();
+        k.tampilData();
+    }
+    
+}
