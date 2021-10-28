5339aac1ad8a4c265dd6e3c93613ce631c78137b
Atmayanti <mayablue74@gmail.com>
2021-10-28 08:09:25 +0700

    add file utama.java

--- src/pertemuan9/percobaan/Manager.java
+++ src/pertemuan9/percobaan/Manager.java
@@ -1,14 +1,51 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pertemuan9.percobaan;
 
-/**
- *
- * @author ATMAYANTI
- */
-public class Manager {
+public class Manager extends Karyawan{
+    private double tunjangan;
+    private String bagian;
+    private Staff st[];
+
+    public void setTunjangan(double tunjangan) {
+        this.tunjangan = tunjangan;
+    }
+
+    public double getTunjangan() {
+        return tunjangan;
+    }
+
+    public void setBagian(String bagian) {
+        this.bagian = bagian;
+    }
+
+    public String getBagian() {
+        return bagian;
+    }
+
+    public void setStaff(Staff[] st) {
+        this.st = st;
+    }
     
+    public void viewStaff(){
+        int i;
+        System.out.println("\n------------------------------");
+        for (int j = 0; j < st.length; j++) {
+            st[j].lihatInfo();
+        }
+        System.out.println("------------------------------");
+    }
+    
+    public void lihatInfo(){
+        System.out.println("Manager\t\t: "+this.getBagian());
+        System.out.println("NIP\t\t: "+this.getNip());
+        System.out.println("Nama\t\t: "+this.getNama());
+        System.out.println("Golongan\t: "+this.getGolongan());
+        System.out.printf("Tunjangan\t: %.0f\n",this.getTunjangan());
+        System.out.printf("Gaji\t\t: %.0f\n",this.getGaji());
+        System.out.printf("Bagian\t\t: "+this.getBagian());
+        this.viewStaff();
+    }
+    
+    public double getGaji(){
+        return super.getGaji()+tunjangan;
+    }
 }
