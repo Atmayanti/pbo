ee29a0cd678148bdcb725e65e17565b24134a727
Atmayanti <mayablue74@gmail.com>
2021-11-04 09:02:27 +0700

    add file orang.java

--- src/pertemuan10/abstractclass/Ikan.java
+++ src/pertemuan10/abstractclass/Ikan.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pertemuan10.abstractclass;
 
-/**
- *
- * @author ATMAYANTI
- */
-public class Ikan {
+public class Ikan extends Hewan {
+
+    @Override
+    public void bergerak() {
+        System.out.println("Berenang dengan SIRIP, \"wush..wush..\"");
+    }
     
 }
--- src/pertemuan10/abstractclass/Kucing.java
+++ src/pertemuan10/abstractclass/Kucing.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pertemuan10.abstractclass;
 
-/**
- *
- * @author ATMAYANTI
- */
-public class Kucing {
+public class Kucing extends Hewan {
+
+    @Override
+    public void bergerak() {
+        System.out.println("Berjalan dengan KAKI, \"Tap..tap..\"");
+    }
     
 }
