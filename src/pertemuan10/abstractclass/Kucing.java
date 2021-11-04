912a78f19d5a16259720cf50646cf696a4d687bb
Atmayanti <mayablue74@gmail.com>
2021-11-04 09:18:25 +0700

    add file program.java

--- src/pertemuan10/abstractclass/Kucing.java
+++ src/pertemuan10/abstractclass/Kucing.java
@@ -2,9 +2,5 @@ package pertemuan10.abstractclass;
 
 public class Kucing extends Hewan {
 
-    @Override
-    public void bergerak() {
-        System.out.println("Berjalan dengan KAKI, \"Tap..tap..\"");
-    }
     
 }
---
+++ src/pertemuan10/abstractclass/Program.java
@@ -0,0 +1,17 @@
+package pertemuan10.abstractclass;
+
+public class Program {
+    public static void main(String[] args) {
+        Kucing kucingKampung = new Kucing();
+        Ikan lumbalumba = new Ikan();
+        
+        Orang ani = new Orang("Ani");
+        Orang budi = new Orang("Budi");
+        
+        ani.peliharaanHewan(kucingKampung);
+        budi.peliharaanHewan(lumbalumba);
+        
+        ani.ajakPeliharaanJalanJalan();
+        budi.ajakPeliharaanJalanJalan();
+    }
+}
