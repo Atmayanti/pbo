9aeffd1f7f1d53ad7ad56ef758e49e0275e9e879
Atmayanti <mayablue74@gmail.com>
2021-11-10 17:01:43 +0700

    add interface ICumlaude

--- src/pertemuan10/abstractclass/Ikan.java
+++ src/pertemuan10/abstractclass/Ikan.java
@@ -3,8 +3,8 @@ package pertemuan10.abstractclass;
 public class Ikan extends Hewan {
 
     @Override
-    public void bergerak() {
-        System.out.println("Berenang dengan SIRIP, \"wush..wush..\"");
-    }
+    public void bergerak(){
+        System.out.println("Berenang dengan SIRIP, \"Wush..Wush..\"");
+    };
     
 }
--- src/pertemuan10/abstractclass/Kucing.java
+++ src/pertemuan10/abstractclass/Kucing.java
@@ -2,5 +2,9 @@ package pertemuan10.abstractclass;
 
 public class Kucing extends Hewan {
 
+    @Override
+    public void bergerak() {
+        System.out.println("Berjalan dengan KAKI, \"Tap..tap..\"");
+    }
     
 }
