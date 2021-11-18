42156fe99bce12786b8547feeb9b4ebe8a3c201d
Atmayanti <mayablue74@gmail.com>
2021-11-18 09:22:28 +0700

    add subclass permanentEmployee

--- src/pertemuan12/percobaan1/InternshipEmployee.java
+++ src/pertemuan12/percobaan1/InternshipEmployee.java
@@ -1,14 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pertemuan12.percobaan1;
-
-/**
- *
- * @author ATMAYANTI
- */
-public class InternshipEmployee {
+public class InternshipEmployee extends Employee{
+    private int length;
     
+    public InternshipEmployee(String name, int length){
+        this.length = length;
+        this.name = name;
+    }
+    
+    public int getLength(){
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+    
+    @Override
+    public String getEmployeeInfo(){
+        String info = super.getEmployeeInfo()+"\n";
+        info += "Registered as internship employee for "+length+" month/s\n";
+        return info;
+    }
 }
